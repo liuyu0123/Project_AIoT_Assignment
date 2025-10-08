87dc427dc5655d8121bd05b5d84e62263cfa0863
LiuYu <liuyu0123@outlook.com>
2025-10-08 17:14:39 +0800

    section4.4.1+2, modify face detect params in python

--- ROS_YuXiang/test4.2_face_detect/src/demo_python_service/demo_python_service/face_detect_node.py
+++ ROS_YuXiang/test4.2_face_detect/src/demo_python_service/demo_python_service/face_detect_node.py
@@ -6,6 +6,7 @@ from cv_bridge import CvBridge
 import cv2
 import face_recognition
 import time
+from rcl_interfaces.msg import SetParametersResult
 
 class FaceDetectorionNode(Node):
     def __init__(self):
@@ -15,8 +16,14 @@ class FaceDetectorionNode(Node):
         self.default_image_path = get_package_share_directory('demo_python_service') \
             + '/resource/default.jpg'
         self.get_logger().info("face detector has been started")
-        self.upsample_times = 1
-        self.model = "hog"
+        # self.upsample_times = 1
+        # self.model = "hog"
+
+        self.declare_parameter("face_locations_upsample_times", 1)
+        self.declare_parameter("face_locations_model", "hog")
+        self.model = self.get_parameter("face_locations_model").value
+        self.upsample_times = self.get_parameter("face_locations_upsample_times").value
+        self.add_on_set_parameters_callback(self.parameters_callback)
 
     def detect_face_callback(self, request, response):
         if request.image.data:
@@ -38,6 +45,15 @@ class FaceDetectorionNode(Node):
         
         return response
     
+    def parameters_callback(self, params):
+        for param in params:
+            self.get_logger().info("{} has been set to {}".format(param.name, param.value))
+            if param.name == "face_locations_upsample_times":
+                self.upsample_times = param.value
+            elif param.name == "face_locations_model":
+                self.model = param.value
+        return SetParametersResult(successful=True)
+
 
 def main(args=None):
     rclpy.init(args=args)
