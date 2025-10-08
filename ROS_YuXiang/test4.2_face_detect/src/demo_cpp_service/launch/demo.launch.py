dc82b207a8dbfe54a8d8e549cde58b29ee94d620
LiuYu <liuyu0123@outlook.com>
2025-10-08 20:24:47 +0800

    section4.6.2, launch parameter

--- ROS_YuXiang/test4.2_face_detect/src/demo_cpp_service/launch/demo.launch.py
+++ ROS_YuXiang/test4.2_face_detect/src/demo_cpp_service/launch/demo.launch.py
@@ -2,12 +2,18 @@ import launch
 import launch_ros
 
 def generate_launch_description():
+    action_declare_arg_max_speed = launch.actions.DeclareLaunchArgument(
+        'launch_max_speed',
+        default_value='2.0',
+        description='max speed of turtle'
+    )
     action_node_turtle_control = launch_ros.actions.Node(
         package='demo_cpp_service',
         executable='turtle_control',
         name='turtle_control_node',
         output='screen',
         parameters=[
+            {'max_speed': launch.substitutions.LaunchConfiguration('launch_max_speed', default_value='2.0')},
         ]
     )
     action_node_patrol_client = launch_ros.actions.Node(
@@ -27,6 +33,7 @@ def generate_launch_description():
         ]
     )
     launch_description = launch.LaunchDescription([
+        action_declare_arg_max_speed,
         action_node_turtle_control,
         action_node_patrol_client,
         action_node_turtlesim_node,
