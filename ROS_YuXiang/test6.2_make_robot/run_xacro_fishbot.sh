0e1fa24b7970698a244f748b77a855e4fa5d2f2a
LiuYu <liuyu0123@outlook.com>
2025-10-10 10:35:45 +0800

    fix bug, now i could determine model in bash file, the reason was  mode_path:=  wrong writen to  model:=

--- ROS_YuXiang/test6.2_make_robot/run_xacro_fishbot.sh
+++ ROS_YuXiang/test6.2_make_robot/run_xacro_fishbot.sh
@@ -1,6 +1,13 @@
 colcon build --packages-select fishbot_description
 source install/setup.bash
-# ros2 launch fishbot_description display_robot.launch.py
-# ros2 launch fishbot_description display_robot.launch.py model:=.../fishbot_description/urdf/fishbot.urdf.xacro
-# ros2 launch fishbot_description display_robot.launch.py model:=.../fishbot_description/urdf/fishbot/fishbot2.urdf.xacro
-ros2 launch fishbot_description display_robot.launch.py model:=$(ros2 pkg prefix fishbot_description)/share/fishbot_description/urdf/fishbot/fishbot2.urdf.xacro
+
+mode_type=3
+if [ $mode_type -eq 1 ]; then
+ros2 launch fishbot_description display_robot.launch.py
+elif [ $mode_type -eq 2 ]; then
+ros2 launch fishbot_description display_robot.launch.py mode_path:=$(ros2 pkg prefix fishbot_description)/share/fishbot_description/urdf/first_robot.urdf.xacro
+elif [ $mode_type -eq 3 ]; then
+ros2 launch fishbot_description display_robot.launch.py mode_path:=$(ros2 pkg prefix fishbot_description)/share/fishbot_description/urdf/fishbot/fishbot.urdf.xacro
+fi
+
+echo
