acc445d3e5ca2fd3087808d3a5deb2c4d7130c39
liuyu <liuyu0123@outlook.com>
2025-10-11 20:54:05 +0800

    section6.5.3, controller

--- ROS_YuXiang/test6.2_make_robot/src/fishbot_description/launch/gazebo_sim.launch.py
+++ ROS_YuXiang/test6.2_make_robot/src/fishbot_description/launch/gazebo_sim.launch.py
@@ -51,9 +51,24 @@ def generate_launch_description():
         output='screen'
     )
 
+    # 加载并激活 fishbot_joint_state_broadcaster 控制器
+    load_joint_state_controller = launch.actions.ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 
+            'active', 'fishbot_joint_state_broadcaster'],
+        output='screen'
+    )
+
+
     return launch.LaunchDescription([
         action_declare_arg_model_path,
         robot_state_publisher_node,
         launch_gazebo,
         spawn_entity_node,
+        # 事件动作，加载并激活 fishbot_joint_state_broadcaster 控制器
+        launch.actions.RegisterEventHandler(
+            event_handler=launch.event_handlers.OnProcessExit(
+                target_action=spawn_entity_node,
+                on_exit=[load_joint_state_controller],
+            )
+        )
     ])
