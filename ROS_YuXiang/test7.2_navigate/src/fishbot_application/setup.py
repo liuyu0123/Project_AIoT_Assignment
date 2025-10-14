527d14af2ced933e84947f5349677100cc21d6ec
liuyu <liuyu0123@outlook.com>
2025-10-14 10:26:53 +0800

    section7.4.3, navigate to pose, python

--- ROS_YuXiang/test7.2_navigate/src/fishbot_application/setup.py
+++ ROS_YuXiang/test7.2_navigate/src/fishbot_application/setup.py
@@ -25,6 +25,7 @@ setup(
     entry_points={
         'console_scripts': [
             'init_robot_pose = fishbot_application.init_robot_pose:main',
+            'nav_to_pose = fishbot_application.nav_to_pose:main',
         ],
     },
 )
