991f4f6ce15827d8c26700b8e028430b5f839e0f
liuyu <liuyu0123@outlook.com>
2025-10-14 16:14:37 +0800

    section7.5-step2, modify setup.py

--- ROS_YuXiang/test7.2_navigate/src/autopatrol_robot/setup.py
+++ ROS_YuXiang/test7.2_navigate/src/autopatrol_robot/setup.py
@@ -10,6 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name+"/config", ['config/patrol_config.yaml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -24,6 +25,7 @@ setup(
     },
     entry_points={
         'console_scripts': [
+            'patrol_node = autopatrol_robot.patrol_node:main',
         ],
     },
 )
