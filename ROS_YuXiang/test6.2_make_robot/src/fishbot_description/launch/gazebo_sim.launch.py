b22d9306b516e8ed9d684751cc73fb3a2442d5ec
LY <61911768+liuyu0123@users.noreply.github.com>
2025-10-17 15:19:19 +0800

    [鱼香ROS2教程]重装Ubuntu22.04学习Gazebo (#6)
    * section6.4.1+2, open model in gazebo
    
    * section6.4.3, define color and friction of robot parts
    
    * section6.4.4, robot gazebo plugin, control robot motion in gazebo
    
    * section6.4.5, laser simulate, un-verify
    
    * section6.4.6, covariance of imu
    
    * section6.4.7, camera depth simulation
    
    * section6.5.2, import ros2_control into gazebo
    
    * section6.5.3, controller
    
    * section6.5.4, force control
    
    * section6.5.5, diff drive controller
    
    * section7.2.1, copy project of section6
    
    * section7.2.1-2, build navigation map
    
    * fix error in section6.4.5, laser simulation
    
    * fix error in project copy, section7.2.1
    
    * section7.2.2, build map
    
    * copy default config of navigation2 from /opt/ros/$ROS_DISTRO/share/nav2_bringup/params/nav2_params.yaml
    
    * section7.3.3, launch navigation
    
    * fix error of map_path, rviz load map
    
    * section7.4.1, create fishbot_application package
    
    * init robot pose by python
    
    * run this after gazebo and rviz to init robot pose
    
    * section7.4.2, get robot real-time pose, un-verify
    
    * section7.4.3, navigate to pose, python
    
    * section7.4.3-2, run nav to pose
    
    * section7.4.3, navigate to pose, c++
    
    * section7.4.4, navigate with waypoints(python)
    
    * section7.5-step1, init package, add patrol_node.py and patrol_config.yaml
    
    * section7.5-step2, modify setup.py
    
    * fix error in autopatrol_robot:patrol_node.py
    
    * section7.5-step3, init package autopatrol_interfaces(c++)
    
    * section7.5-step4, add srv in autopatrol_interfaces
    
    * section7.5-step5, add speaker in interfaces, add launch file and modify setup.py
    
    * section7.5-step6, add image save in autopatrol_robot:patrol_node.py
    
    * add run bash file, fix some error
    
    * add world folder, which ignored previous
    
    * modify .gitignore
    
    * add maps in section7
    
    * fix error of section7.4.3, navigate to single point goal with interfaces
    
    * add bash file of section7.4.4, navigate of waypoints by interfaces
    
    * fix bug in section7.5, which led gazebo and rviz un-align.
    
    * add some bash

---
+++ ROS_YuXiang/test6.2_make_robot/src/fishbot_description/launch/gazebo_sim.launch.py
@@ -0,0 +1,100 @@
+import launch
+import launch_ros
+from ament_index_python.packages import get_package_share_directory
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+import os
+
+def generate_launch_description():
+    # 获取默认路径
+    robot_name_in_model = "fishbot"
+    urdf_tutorial_path = get_package_share_directory('fishbot_description')
+    default_model_path = os.path.join(urdf_tutorial_path, 'urdf', 'fishbot', 'fishbot.urdf.xacro')
+    default_world_path = os.path.join(urdf_tutorial_path, 'world', 'custom_room.world')
+    # 声明launch参数
+    action_declare_arg_model_path = launch.actions.DeclareLaunchArgument(
+        name='model',
+        default_value=str(default_model_path),
+        description='the path of model urdf'
+    )
+    # 获取文件内容生成新的参数
+    robot_description = launch_ros.parameter_descriptions.ParameterValue(
+        launch.substitutions.Command(
+            ['xacro ', launch.substitutions.LaunchConfiguration('model')]),
+        value_type=str)
+    robot_state_publisher_node = launch_ros.actions.Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='both',
+        parameters=[
+            {'robot_description': robot_description}
+        ]
+    )
+    # 通过 IncludeLaunchDescription 引入launch文件
+    launch_gazebo = launch.actions.IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')
+        ),
+        launch_arguments={
+            'world': default_world_path,
+            'verbose': 'true'
+        }.items()
+    )
+    # 请求 gazebo 加载机器人
+    spawn_entity_node = launch_ros.actions.Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=[
+            '-entity', robot_name_in_model,
+            '-topic', '/robot_description'
+        ],
+        output='screen'
+    )
+
+    # 加载并激活 fishbot_joint_state_broadcaster 控制器
+    load_joint_state_controller = launch.actions.ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 
+            'active', 'fishbot_joint_state_broadcaster'],
+        output='screen'
+    )
+
+    # 加载并激活 fishbot_effort_controller 控制器
+    load_fishbot_effort_controller = launch.actions.ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 
+            'active', 'fishbot_effort_controller'],
+        output='screen'
+    )
+
+    # 加载并激活 fishbot_diff_drive_controller 控制器
+    load_fishbot_diff_drive_controller = launch.actions.ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 
+            'active', 'fishbot_diff_drive_controller'],
+        output='screen'
+    )
+
+
+    return launch.LaunchDescription([
+        action_declare_arg_model_path,
+        robot_state_publisher_node,
+        launch_gazebo,
+        spawn_entity_node,
+        # 事件动作，加载并激活 fishbot_joint_state_broadcaster 控制器
+        launch.actions.RegisterEventHandler(
+            event_handler=launch.event_handlers.OnProcessExit(
+                target_action=spawn_entity_node,
+                on_exit=[load_joint_state_controller],
+            )
+        ),
+        launch.actions.RegisterEventHandler(
+            event_handler=launch.event_handlers.OnProcessExit(
+                target_action=load_joint_state_controller,
+                on_exit=[load_fishbot_effort_controller],
+            )
+        ),
+        launch.actions.RegisterEventHandler(
+            event_handler=launch.event_handlers.OnProcessExit(
+                target_action=load_fishbot_effort_controller,
+                on_exit=[load_fishbot_diff_drive_controller],
+            )
+        ),
+    ])
