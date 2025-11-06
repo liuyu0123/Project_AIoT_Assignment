57422b44f81bf9ee6479a8bba6c0550decbeac01
LY <liuyu0123@outlook.com>
2025-11-06 14:14:44 +0800

    fix check interface

--- ROS_YuXiang/learn1_math_add/run_interface.sh
+++ ROS_YuXiang/learn1_math_add/run_interface.sh
@@ -1,4 +1,4 @@
 colcon build --packages-select interface_test
 source install/setup.bash
-ros2 interface list | grep number
+ros2 interface list | grep Number
 ros2 interface show interface_test/msg/NumberAdd
