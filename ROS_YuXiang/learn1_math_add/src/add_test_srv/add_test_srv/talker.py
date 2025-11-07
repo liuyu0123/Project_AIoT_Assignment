506021d7ba4d8bdae1067cf3425acf5b79f526ae
LY <liuyu0123@outlook.com>
2025-11-07 17:19:32 +0800

    add the weight_factor by parameter

--- ROS_YuXiang/learn1_math_add/src/add_test_srv/add_test_srv/talker.py
+++ ROS_YuXiang/learn1_math_add/src/add_test_srv/add_test_srv/talker.py
@@ -5,10 +5,14 @@ from interface_test.srv import NumberAddResult
 class Talker(Node):
     def __init__(self):
         super().__init__('add_server')
+        self.declare_parameter('weight_a', 1.0)
+        self.declare_parameter('weight_b', 1.0)
         self.service = self.create_service(NumberAddResult, 'add_two_number_result', self.add_two_number_result)
         self.get_logger().info('add_server is ready')
     def add_two_number_result(self, request, response):
-        response.result = request.a + request.b
+        weight_a = self.get_parameter('weight_a').value
+        weight_b = self.get_parameter('weight_b').value
+        response.result = weight_a*request.a + weight_b*request.b
         self.get_logger().info(f'收到请求: {request.a} + {request.b} = {response.result}')
         return response
     
