fb74c25e981d730871d5c8c498406a14033bb465
LY <liuyu0123@outlook.com>
2025-11-08 17:03:32 +0800

    fix bug

--- ROS_YuXiang/learn1_math_add/launch_add_srv.sh
+++ ROS_YuXiang/learn1_math_add/launch_add_srv.sh
@@ -1,4 +1,4 @@
 colcon build --packages-select add_test_srv
 source install/setup.bash
 # ros2 launch add_test_srv test.launch.py
-ros2 launch add_test_srv test.launch.py weight_factor_a:=2.0 weight_factor_b:=2.0
+ros2 launch add_test_srv test.launch.py talker.weight_a:=2.0 talker.weight_b:=2.0
