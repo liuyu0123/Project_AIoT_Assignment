7917f4cbb08fed7922a1c61913db8cd429dafa02
LY <liuyu0123@outlook.com>
2025-11-13 14:40:30 +0800

    modify code of helloworld

--- ROS_YuXiang/platformio/example01_helloworld/src/main.cpp
+++ ROS_YuXiang/platformio/example01_helloworld/src/main.cpp
@@ -5,11 +5,14 @@ int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // int result = myFunction(2, 3);
+  Serial.begin(115200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.printf("Hello World %d\n");
+  delay(1000);
 }
 
 // put function definitions here:
