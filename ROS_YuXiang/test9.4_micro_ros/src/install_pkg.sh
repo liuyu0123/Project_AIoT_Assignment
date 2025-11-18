f03ac06327994e6ef58162c965dc43d374dc632c
LY <liuyu0123@outlook.com>
2025-11-18 22:21:05 +0800

    add lidar packages create

--- ROS_YuXiang/test9.4_micro_ros/src/install_pkg.sh
+++ ROS_YuXiang/test9.4_micro_ros/src/install_pkg.sh
@@ -4,3 +4,5 @@ cd "$(dirname "$0")" || exit
 # 
 git clone https://github.com/micro-ROS/micro-ROS-Agent.git -b $ROS_DISTRO
 git clone https://github.com/micro-ROS/micro_ros_msgs.git -b $ROS_DISTRO
+git clone https://github.com/fishros/ros_serial2wifi.git
+git clone https://github.com/fishros/ydlidar_ros2.git -b fishbot
