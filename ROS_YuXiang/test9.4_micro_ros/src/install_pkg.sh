49ccbc3b11b7217d22b385d1bd13875cd46e3319
LY <liuyu0123@outlook.com>
2025-11-22 12:34:48 +0800

    add pkg create in yuxiang-ros

--- ROS_YuXiang/test9.4_micro_ros/src/install_pkg.sh
+++ ROS_YuXiang/test9.4_micro_ros/src/install_pkg.sh
@@ -6,3 +6,6 @@ git clone https://github.com/micro-ROS/micro-ROS-Agent.git -b $ROS_DISTRO
 git clone https://github.com/micro-ROS/micro_ros_msgs.git -b $ROS_DISTRO
 git clone https://github.com/fishros/ros_serial2wifi.git
 git clone https://github.com/fishros/ydlidar_ros2.git -b fishbot
+
+ros2 pkg create fishbot_description
+ros2 pkg create fishbot_bringup
