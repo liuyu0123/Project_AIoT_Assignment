c4c598b0f942541dcfb82c91266c739b911fa7f3
LY <liuyu0123@outlook.com>
2025-11-23 17:05:57 +0800

    add code comments in micro-ros

--- esp32_s3/proj_micro_ros/src/main.cpp
+++ esp32_s3/proj_micro_ros/src/main.cpp
@@ -16,16 +16,20 @@ rcl_node_t node;
 
 void setup()
 {
+    // 配置wifi，用于和上位机通信
     IPAddress agent_ip;
     agent_ip.fromString("192.168.0.101");
     char WIFI_NAME[] = "TP-LINK_3371";
     char WIFI_PASSWORD[] = "qwe12345";
     set_microros_wifi_transports(WIFI_NAME, WIFI_PASSWORD, agent_ip, 8888);
-    delay(2000);
-    allocator = rcl_get_default_allocator();
-    rclc_support_init(&support, 0, NULL, &allocator);
+    delay(2000); // 给一个延时，等待wifi连接成功
 
-    rclc_node_init_default(&node, "esp32_node", "", &support);
+    // 初始化 micro-ros 环境
+    allocator = rcl_get_default_allocator();
+    rclc_support_init(&support, 0, NULL, &allocator); //argc=0, argv=NULL
+
+    // 创建节点
+    rclc_node_init_default(&node, "esp32_node", "", &support); //esp32_node是节点（node）名称
 
     // rclc_publisher_init_default(
     //     &pub, &node,
@@ -39,8 +43,11 @@ void setup()
     //     ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
     //     "esp32_counter", &qos);
 
+    // 初始化发布者
+    // 参数：1. publisher， 2. node， 3. 消息类型， 4. topic名称
     rclc_publisher_init_best_effort(&pub, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32), "/esp32_counter");
 
+    // 初始化定时器+回调函数，定时器与回调永远一对一，而回调可以对应多个 publisher
     rclc_timer_init_default(&timer, &support, RCL_MS_TO_NS(50),
                             [](rcl_timer_t *, int64_t)
                             {
