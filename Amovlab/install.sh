c6b94551a55d7291e6b6a3f82ef1566c6b1987ec
LY-Raspi3B <liuyu0123@outlook.com>
2025-12-05 08:21:48 +0000

    fix install.sh, add colavoidance.py

--- Amovlab/install.sh
+++ Amovlab/install.sh
@@ -1,6 +1,6 @@
 set -e
 board=RaspberryPi3B
-
+sudo apt update
 sudo apt install python3-pip python3-dev python3-venv -y
 
 # python_edition=python3 #python3.9
@@ -9,11 +9,12 @@ sudo apt install python3-pip python3-dev python3-venv -y
 # python3 -m pip venv dronekit-env
 # source dronekit-env/bin/activate
 
-if [ $board -eq "RaspberryPi5" ]; then
+if [ $board = "RaspberryPi5" ]; then
 
 env_name=dronekit-env-py39
 if [ -d "$env_name" ]; then
 echo "$env_name 开发环境已存在"
+source $env_name/bin/activate
 else
 echo "创建 $env_name 开发环境..."
 python3.9 -m venv $env_name   # 用 3.9 创建新环境
@@ -27,14 +28,17 @@ pip install --upgrade pymavlink
 pip install numpy opencv-python
 pip install pyserial
 
-elif [ $board -eq "RaspberryPi3B" ]; then
+elif [ $board = "RaspberryPi3B" ]; then
 #树莓派3B，ubuntu20.04，系统python版本3.8
+sudo apt install libgl1-mesa-glx -y
+
 env_name=dronekit-env
 if [ -d "$env_name" ]; then
 echo "$env_name 开发环境已存在"
+source $env_name/bin/activate
 else
 echo "创建 $env_name 开发环境..."
-python3.9 -m venv $env_name   # 用 3.9 创建新环境
+python3 -m venv $env_name   # 用 3.9 创建新环境
 source $env_name/bin/activate
 fi
 
@@ -47,7 +51,8 @@ pip install future -i https://pypi.tuna.tsinghua.edu.cn/simple
 pip install --upgrade pymavlink
 pip install numpy opencv-python -i https://pypi.tuna.tsinghua.edu.cn/simple
 pip install pyserial -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip pigpio
 
 else
-echo "board not support"
+echo "开发板名称错误，请检查..."
 fi
