ec124ca953728f2a2b811f40e4385ea926393906
LY <liuyu0123@outlook.com>
2025-12-15 16:23:04 +0800

    yolo world model, determine text word in camera

---
+++ BPI/yolo-world/python/python_env_install.ps1
@@ -0,0 +1,3 @@
+python -m venv yolo-world
+yolo-world\Scripts\Activate.ps1
+pip install -r requirements.txt
---
+++ BPI/yolo-world/python/run.ps1
@@ -0,0 +1,3 @@
+yolo-world\Scripts\Activate.ps1
+# python test_yolo-world.py --use-camera --classes people
+python test_yolo-world.py --use-camera --classes people telephone pen box mouse cup face
