f03a51250eab4a4e295ad423df672e59d750ab58
liuyu <liuyu0123@outlook.com>
2026-01-16 16:24:57 +0800

    launch yolov5 with pipeline together

--- WelaBoat_ws/run_pipeline.sh
+++ WelaBoat_ws/run_pipeline.sh
@@ -1,3 +1,7 @@
 colcon build
+source ~/miniconda3/bin/activate yolov5_env
+python -m colcon build --packages-select yolov5_detector fastscnn_segmenter --symlink-install
+conda deactivate
+conda deactivate
 source install/setup.bash
 ros2 launch welaboat_bringup welaboat.launch.py
