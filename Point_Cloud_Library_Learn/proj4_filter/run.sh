3a760ccc3e178083fd84d6d3a2cc6f97fe2e636e
liuyu <liuyu0123@outlook.com>
2026-01-19 18:19:30 +0800

    step8, generate ros2 map, include .pgm and .yaml files

--- Point_Cloud_Library_Learn/proj4_filter/run.sh
+++ Point_Cloud_Library_Learn/proj4_filter/run.sh
@@ -1,3 +1,6 @@
+# !/bin/bash
+set -e
+
 # 创建构建目录
 if [ ! -d "build" ]; then
   echo "build已存在，跳过创建..."
@@ -11,13 +14,13 @@ cmake ..
 # 编译
 make -j4
 
-# 点云去噪
+# step2. 点云去噪
 pcd_raw=/home/riba/GitProject/LIUYU/Point_Cloud_Library_Learn/data/0000000000.pcd
 pcd_filtered=/home/riba/GitProject/LIUYU/Point_Cloud_Library_Learn/data/step2_filtered.pcd
 ./step2_denoise $pcd_raw $pcd_filtered
 
 
-# 点云分割：水面/河岸
+# step3. 点云分割：水面/河岸
 pcd_water=/home/riba/GitProject/LIUYU/Point_Cloud_Library_Learn/data/step3_water.pcd
 pcd_shore=/home/riba/GitProject/LIUYU/Point_Cloud_Library_Learn/data/step3_shore.pcd
 ./step3_segment_water $pcd_raw $pcd_water $pcd_shore
@@ -30,7 +33,7 @@ pcd_shore=/home/riba/GitProject/LIUYU/Point_Cloud_Library_Learn/data/step3_shore
 # pcl_viewer step3_water.pcd step3_shore.pcd
 
 
-# 点云聚类
+# step4. 点云聚类
 pcd_clusters=/home/riba/GitProject/LIUYU/Point_Cloud_Library_Learn/data/step4_shore_clusters.pcd
 ./step4_cluster_shore $pcd_shore $pcd_clusters
 # pcl_viewer step3_shore.pcd -fc 255,255,255 -ps 4
@@ -40,22 +43,32 @@ pcd_clusters=/home/riba/GitProject/LIUYU/Point_Cloud_Library_Learn/data/step4_sh
 # CloudCompare step4_shore_clusters.pcd
 # 在CloudCompare中手动将pcd拖入
 
-# 点云投影到2D平面
+# step5. 点云投影到2D平面
 pcd_projected=/home/riba/GitProject/LIUYU/Point_Cloud_Library_Learn/data/step5_projected.pcd
 ./step5_project_2d $pcd_clusters $pcd_projected
 
 
-# 拟合出左右河岸线
+# step6. 拟合出左右河岸线
 pcd_left_line=/home/riba/GitProject/LIUYU/Point_Cloud_Library_Learn/data/step6_left_shore_line.pcd
 pcd_right_line=/home/riba/GitProject/LIUYU/Point_Cloud_Library_Learn/data/step6_right_shore_line.pcd
 # 沿 X 轴航行（你的场景）
-./step6_extract_centerlines2 $pcd_projected $pcd_left_line $pcd_right_line --axis x
+# 方法1（kimi）可运行
+# ./step6_extract_centerlines $pcd_projected $pcd_left_line $pcd_right_line --axis x
+# 方法2（阿里千问）
+./step6_extract_centerlines2 $pcd_projected $pcd_left_line $pcd_right_line
 # 如果以后需要沿 Y 轴（可选）
 # ./step6_extract_centerlines $pcd_projected $pcd_left_line $pcd_right_line --axis y
 # pcl_viewer step6_left_shore_line.pcd -fc 255,0,0 -ps 8 step6_right_shore_line.pcd -fc 0,255,0 -ps 8
 
 
-# 插值补齐河岸线
+# step7. 插值补齐河岸线
 interpolate_output_path=/home/riba/GitProject/LIUYU/Point_Cloud_Library_Learn/data/
 ./step7_interpolate_shores $pcd_left_line $pcd_right_line $interpolate_output_path
 # pcl_viewer left_interp.pcd -fc 255,0,0 -ps 8 right_interp.pcd -fc 0,255,0 -ps 8
+
+
+# step8. 生成地图
+./step8_generate_occupancy_grid \
+  $interpolate_output_path/left_interp.pcd \
+  $interpolate_output_path/right_interp.pcd \
+  $interpolate_output_path/river_map
