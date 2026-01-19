676d9072427003e5c96474bfb9a09432986ab925
liuyu <liuyu0123@outlook.com>
2026-01-19 11:11:23 +0800

    move data

--- Point_Cloud_Library_Learn/proj3_load_csv/run.sh
+++ Point_Cloud_Library_Learn/proj3_load_csv/run.sh
@@ -1,5 +1,5 @@
-csv_path=/home/riba/GitProject/LIUYU/Point_Cloud_Library_Learn/proj3_load_csv/data/0000000000.csv
-pcd_path=/home/riba/GitProject/LIUYU/Point_Cloud_Library_Learn/proj3_load_csv/data/0000000000.pcd
+csv_path=/home/riba/GitProject/LIUYU/Point_Cloud_Library_Learn/data/0000000000.csv
+pcd_path=/home/riba/GitProject/LIUYU/Point_Cloud_Library_Learn/data/0000000000.pcd
 python3 csv2pcd.py $csv_path $pcd_path
 
 point_color_white=255,255,255
