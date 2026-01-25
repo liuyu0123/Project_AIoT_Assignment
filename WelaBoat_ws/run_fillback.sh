c097db775a9a4df4f1d039d38bd13a3c93fa7a21
liuyu <liuyu0123@outlook.com>
2026-01-25 17:20:05 +0800

    modify

--- WelaBoat_ws/run_fillback.sh
+++ WelaBoat_ws/run_fillback.sh
@@ -1,11 +1,16 @@
-old_bag=old.db3
-new_debug_bag=new_debug_bag
+# old_bag=old.db3
+# new_debug_bag=new_debug_bag
+old_bag=/home/riba/GitProject/LIUYU/WelaBoat_ws/record/Target3DNew/rosbag2_2026_01_24-16_31_52/rosbag2_2026_01_24-16_31_52_0.mcap
+new_debug_bag=/home/riba/GitProject/LIUYU/WelaBoat_ws/record/record1
+
+
 
 # 先启动节点，在开终端运行本脚本
 
-ros2 bag play $old_bag --clock &
+ros2 bag play $old_bag --clock --disable-keyboard  &
 ros2 bag record \
   /fused/freespace_debug \
   /fused/objects_debug \
   /fused/objects_markers_debug \
-  -o $new_debug_bag
+  -o $new_debug_bag \
+  --storage mcap
