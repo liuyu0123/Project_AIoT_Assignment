effcd848439f298bac6d47ceee78cb5b55936282
liuyu <liuyu0123@outlook.com>
2026-01-26 20:58:28 +0800

    optimize conda_env_create.sh

--- WelaBoat_ws/conda_env_create.sh
+++ WelaBoat_ws/conda_env_create.sh
@@ -7,13 +7,26 @@ bash Miniconda3-latest-Linux-x86_64.sh
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-aarch64.sh
 bash Miniconda3-latest-Linux-aarch64.sh
 
+
 # 2. 构建虚拟环境 yolov5_env
 conda create -n yolov5_env -y python=3.10
 conda activate yolov5_env
 cd ~/yolov5
 pip install -r requirements.txt
 pip install colcon-common-extensions
+conda deactivate
+
+
+# 3. [暂时不需要]构建虚拟环境 lidar_env
+conda create -n lidar_env -y python=3.10
+conda activate lidar_env
+pip install open3d
+conda deactivate
+
+
+# 4. 修复 yolov5_env 的 opencv 开发环境
 # 处理 yolov5 环境下 numpy 与 opencv 版本冲突的问题
+source ~/miniconda3/bin/activate yolov5_env
 # step1.安装 numpy=1.X
 pip install numpy==1.26.4
 # step2.卸载 opencv
@@ -21,10 +34,3 @@ pip uninstall opencv-python -y
 # step33. 创建软链接（关键！），将系统 opencv 软链到 conda 环境中
 ln -sf /usr/lib/python3/dist-packages/cv2*.so \
        ~/miniconda3/envs/yolov5_env/lib/python3.10/site-packages/cv2.so
-
-# 3. [暂时不需要]构建虚拟环境 lidar_env
-conda create -n lidar_env -y python=3.10
-conda activate lidar_env
-pip install open3d
-
-# 4. 修复 yolov5_env 的 opencv 开发环境
