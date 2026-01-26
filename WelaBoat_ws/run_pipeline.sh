7adf38c2aa7c5e8151f42fa33d74ec1c43b5a48f
liuyu <liuyu0123@outlook.com>
2026-01-26 11:35:46 +0800

    modify, debug the freespace_shoreline

--- WelaBoat_ws/run_pipeline.sh
+++ WelaBoat_ws/run_pipeline.sh
@@ -1,8 +1,15 @@
+if_launch=$1
+
 colcon build
 source ~/miniconda3/bin/activate yolov5_env
 python -m colcon build --packages-select yolov5_detector fastscnn_segmenter --symlink-install
+source ~/miniconda3/bin/activate lidar_env
+python -m colcon build --packages-select freespace_shoreline --symlink-install
 conda deactivate
 conda deactivate
 source install/setup.bash
-ros2 launch welaboat_bringup welaboat.launch.py
+
+if [ $if_launch -eq 1 ]; then
+    ros2 launch welaboat_bringup welaboat.launch.py
+fi
 # ros2 launch welaboat_bringup welaboat_visionOnly.launch.py
