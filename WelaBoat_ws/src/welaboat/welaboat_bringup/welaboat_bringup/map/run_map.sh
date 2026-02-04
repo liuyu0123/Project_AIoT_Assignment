56abb5ab24c98bbfe7062eaa20495b884dbb4229
liuyu <liuyu0123@outlook.com>
2026-02-04 22:21:09 +0800

    fix run_map

--- WelaBoat_ws/src/welaboat/welaboat_bringup/welaboat_bringup/map/run_map.sh
+++ WelaBoat_ws/src/welaboat/welaboat_bringup/welaboat_bringup/map/run_map.sh
@@ -3,12 +3,16 @@
 #2 map_server
 ros2 run nav2_map_server map_server \
   --ros-args -p yaml_filename:=/path/to/map.yaml
+ros2 run nav2_map_server map_server --ros-args -p yaml_filename:=/home/riba/GitProject/LIUYU/WelaBoat_ws/src/welaboat/welaboat_bringup/welaboat_bringup/map/map.yaml
 
 #3 configure / activate
 ros2 lifecycle set /map_server configure
 ros2 lifecycle set /map_server activate
 
-ros2 topic echo /map
+ros2 topic echo /map --once
+
+#fix1 map-odom transform
+ros2 run tf2_ros static_transform_publisher 0 0 0 0 0 0 map odom
 
 #4 global_frame: map [in nav2_params.yaml]
 
@@ -16,5 +20,3 @@ ros2 topic echo /map
 
 #6 check goal in rviz
 
-#fix1 map-odom transform
-ros2 run tf2_ros static_transform_publisher 0 0 0 0 0 0 map odom
