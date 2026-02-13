3d3811e5d2a117bf5a6c3ad5e1a1afaa6c78588a
liuyu <liuyu0123@outlook.com>
2026-02-13 11:16:34 +0800

    fix localization setup.py

--- WelaBoat_ws/src/localization/welaboat_localization/setup.py
+++ WelaBoat_ws/src/localization/welaboat_localization/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'welaboat_localization'
 
@@ -10,6 +12,13 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # ('share/'+package_name+'/launch', ['*.launch.py']),
+        # (os.path.join('share', package_name, 'launch'),
+        #  glob(os.path.join('welaboat_bringup', 'launch', '*.launch.py'))),
+        (os.path.join('share', package_name, 'launch'),
+         glob(os.path.join('launch', '*.launch.py'))),
+        (os.path.join('share', package_name, 'config'),
+         glob(os.path.join('config', '*.yaml'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
