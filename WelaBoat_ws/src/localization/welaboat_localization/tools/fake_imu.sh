c506536d108e856fbeb5b15cab4889b221f35556
liuyu <liuyu0123@outlook.com>
2026-02-13 11:38:03 +0800

    fix fake imu shell

--- WelaBoat_ws/src/localization/welaboat_localization/tools/fake_imu.sh
+++ WelaBoat_ws/src/localization/welaboat_localization/tools/fake_imu.sh
@@ -1,6 +1,18 @@
-ros2 topic pub /imu/data sensor_msgs/msg/Imu \
-"{header: {frame_id: base_link},
-  orientation: {x: 0.0, y: 0.0, z: 0.0, w: 1.0},
-  angular_velocity: {z: 0.0},
-  linear_acceleration: {x: 0.0, y: 0.0, z: 9.8}}" \
--r 10
+ros2 topic pub /imu/data sensor_msgs/msg/Imu "
+header:
+  stamp:
+    sec: 1770953833
+    nanosec: 0
+  frame_id: 'base_link'
+orientation:
+  x: 0.0
+  y: 0.0
+  z: 0.0
+  w: 1.0
+angular_velocity:
+  z: 0.0
+linear_acceleration:
+  x: 0.0
+  y: 0.0
+  z: 9.8
+" -r 10
